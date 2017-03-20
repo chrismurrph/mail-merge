ea62d11755be35ce2d9dc08a20c728d9b8a3802e
Chris Murphy <chris@strandz.org>
2017-03-20 20:48:18 +1030

    Image alignments are as good as I can get them

--- src/cv/common.clj
+++ src/cv/common.clj
@@ -1,17 +1,22 @@
 (ns cv.common
   (:require [clojure.java.io :as io]))
 
-(defn image-here [image-file-name scale]
-  (let [img (-> image-file-name io/resource)]
-    [:image {:scale scale
-             :align :center
-             :valign :middle}
-     img]))
+(defn image-here
+  ([image-file-name scale x y]
+   (let [img (-> image-file-name io/resource)]
+     [:chunk {:x x :y y} [:image {:scale  scale
+                                   :align  :center
+                                   :valign :middle}
+                           img]]))
+  ([image-file-name scale]
+    (image-here image-file-name scale 0 -2))
+  ([image-file-name scale x]
+   (image-here image-file-name scale x -2)))
 
 (defn flattened-image-here [image-file-name scale]
   (let [img (-> image-file-name io/resource)]
     [:image {:xscale (/ scale 90)
-             :yscale (/ scale 130)
+             :yscale (/ scale 90)
              :align  :center
              :valign :middle}
      img]))
--- src/cv/core.clj
+++ src/cv/core.clj
@@ -15,39 +15,45 @@
 
 (defn write-pdf-file [cv file-name]
   (pdf/pdf
-    [{:top-margin    5
-      :bottom-margin 5}
+    [{:pages true
+      :top-margin    25
+      :bottom-margin 30}
      cv]
     file-name))
 
 (def cell-props {:valign :top :align :left})
 (def top-props {:valign :top :align :center})
+(def middle-props {:valign :middle :align :center})
+(def middle-left-props {:valign :middle :align :left})
 
 ;;
 ;; Only the Github link actually works. No time to fix this (or put pictures)
 ;;
 (defn short-version [contact-str]
   (cond
-    (or (clojure.string/index-of contact-str "stackexchange")
-        (clojure.string/index-of contact-str "stackoverflow")) "StackOverflow"
-    (clojure.string/index-of contact-str "github") "GitHub"
-    (clojure.string/index-of contact-str "linkedin") "LinkedIn"
+    (or (s/index-of contact-str "stackexchange")
+        (s/index-of contact-str "stackoverflow")) "StackOverflow"
+    (s/index-of contact-str "github") "GitHub"
+    (s/index-of contact-str "linkedin") "LinkedIn"
+    (s/index-of contact-str "clojurians") "Clojurians Slack"
     :default contact-str))
 
 ;;
 ;; Works but going to take too much time with every image being a slightly different size
 ;;
-(defn create-contacts-table-with-image [contact-links]
-  (let [img-fn c/flattened-image-here]
+(defn create-contacts-table-with-images [contact-links]
+  (let [img-fn c/image-here]
     [:pdf-table
      {:width-percent 100
       :cell-border   false}
      [1 15]
-     [[:pdf-cell cell-props (img-fn "github-2.png" 1.6)] [:pdf-cell cell-props [:paragraph (first contact-links)]]]
-     [[:pdf-cell cell-props (img-fn "github-2.png" 1.6)] [:pdf-cell cell-props [:paragraph (second contact-links)]]]
-     [[:pdf-cell cell-props (img-fn "github-2.png" 1.6)] [:pdf-cell cell-props [:paragraph (u/third contact-links)]]]]))
+     [[:pdf-cell middle-props (img-fn "github-2.png" 1.6 0 -3)] [:pdf-cell middle-left-props [:paragraph (first contact-links)]]]
+     [[:pdf-cell middle-props (img-fn "apple-touch-icon.png" 11)] [:pdf-cell middle-left-props [:paragraph (second contact-links)]]]
+     [[:pdf-cell middle-props (img-fn "linkedin-button.png" 7 2 0)] [:pdf-cell middle-left-props [:paragraph (u/third contact-links)]]]
+     [[:pdf-cell middle-props (img-fn "slack.png" 3 -3)] [:pdf-cell middle-left-props [:paragraph (u/fourth contact-links)]]]
+     ]))
 
-(defn create-contacts-table [contact-links]
+(defn create-contacts-table-no-images [contact-links]
   [:pdf-table
    {:width-percent 100
     :cell-border   false}
@@ -55,38 +61,40 @@
    [[:pdf-cell cell-props [:paragraph (first contact-links)]]]
    [[:pdf-cell cell-props [:paragraph (second contact-links)]]]
    [[:pdf-cell cell-props [:paragraph (u/third contact-links)]]]
-   ]
-  )
+   [[:pdf-cell cell-props [:paragraph (u/fourth contact-links)]]]])
+
+(def create-contacts-table create-contacts-table-with-images)
 
 (defn create-intro [name contacts address keywords libs]
   (assert (string? contacts))
-  (let [details (s/split contacts #",")
+  (let [props middle-left-props
+        details (s/split contacts #",")
         links (->> details
                    (map read-string)
                    (map (juxt (comp str first) (comp str second))))
-        _ (println "links: " links)
+        ;_ (println "links: " links)
         contact-links (mapv (fn [[link user-id]]
                               [:anchor {:target link} (str (short-version link) " (" user-id ")")]) links)]
-    (println contact-links)
+    ;(println contact-links)
     [:pdf-table {
                  ;:cell-border  true
                  ;:horizontal-align :left
                  :width-percent 100
                  }
      [1 5]
-     [[:pdf-cell cell-props "Name"] [:pdf-cell cell-props name]]
-     [[:pdf-cell cell-props "Links"] [:pdf-cell cell-props (create-contacts-table contact-links)]]
-     [[:pdf-cell cell-props "Address"] [:pdf-cell cell-props address]]
-     [[:pdf-cell cell-props "Experience"] [:pdf-cell cell-props keywords]]
-     [[:pdf-cell cell-props "Libraries"] [:pdf-cell cell-props libs]]
+     [[:pdf-cell props "Name"] [:pdf-cell props name]]
+     [[:pdf-cell cell-props "Links"] [:pdf-cell props (create-contacts-table contact-links)]]
+     [[:pdf-cell props "Address"] [:pdf-cell props address]]
+     [[:pdf-cell props "Experience"] [:pdf-cell props keywords]]
+     [[:pdf-cell props "Libraries"] [:pdf-cell props libs]]
      ]))
 
 (defn image-table [name contact-links address keywords libs image-file-name]
   [:pdf-table
    {:width-percent 100
     :cell-border   true}
-   [9 2]
-   [(create-intro name contact-links address keywords libs) (c/image-here image-file-name 30)]])
+   [7.25 2.02]
+   [(create-intro name contact-links address keywords libs) (c/image-here image-file-name 36.7)]])
 
 (defn create-job-row [{:keys [month-from year-from month-to year-to org position]}]
   (assert (string? month-to))
@@ -118,10 +126,14 @@
 (defn get-jobs []
   (read-string (slurp (io/resource "cv-jobs.edn"))))
 
+;; http://www.seasoft.com.au/atmosphere/
+(def atmosphere-link "http://www.seasoft.com.au/atmosphere/")
+
 (defn produce-cv []
-  (let [paragraphs (->> cv-in-file-name
+  (let [paragraphs' (->> cv-in-file-name
                         u/file-name->lines
                         (mapv cc/create-spaced-paragraph))
+        paragraphs (->> paragraphs' (u/insert-at 3 [:paragraph [:anchor {:target atmosphere-link} atmosphere-link] [:spacer]]))
         summary-info (->> cv-summary-in-file-name
                           u/file-name->lines)]
     (let [cv (make-cv summary-info (get-jobs) paragraphs)
