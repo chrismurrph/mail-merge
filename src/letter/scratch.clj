a03cbde73fd397b8df8cf74d02e2146eb9103826
Chris Murphy <chris@strandz.org>
2017-04-04 14:33:08 +0930

    Hmm - stuff like my CV text was in the repo, now not

--- src/letter/scratch.clj
+++ src/letter/scratch.clj
@@ -12,3 +12,28 @@
 (comment
   (defn x-1 []
     (transform {:age 0})))
+
+(def switchvar (atom 1))
+(def items [1 2 3])
+
+(defn listitems [items]
+  [:ul
+   (for [item items
+         idx (range (count items))]
+     ^{:key item} [:li {:class
+                        (if (and (odd? idx) (odd? @switchvar))
+                          "active"
+                          "inactive")
+                        } "Item " item])])
+
+(defn x-10 []
+  (listitems items))
+
+(def stars [1 2 3 4 5])
+(def planets [:a :b :c :d :e :f :h :i :j :k :l :m])
+
+(defn gen-universe []
+  (let [max-num-planets 3
+        s-picked (mapcat #(repeat (inc (rand-int max-num-planets)) %) stars)]
+    (map #(list %1 %2) s-picked planets)))
+
